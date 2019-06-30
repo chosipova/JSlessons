dc7b1b197eb7b9a72d0c0769785174bea56b3595
Christina Osipova <chosipova@gmail.com>
2019-06-30 22:49:15 +0300

    lesson 2

--- lesson1/script.js
+++ lesson1/script.js
@@ -1,9 +1,21 @@
-var money;
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+let money = 30300;
+let income = 'frilans';
+let addExpenses ='300, 7000, 4000';
+let deposit = false;
+let mission = 120000;
+let period = 12;
 
-console.log("lesson01");
-alert("Hello, world!")
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(income.length);
+
+console.log('Период', period, 'месяцев');
+console.log('Цель заработать', mission, 'рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay;
+budgetDay = money / 30;
+console.log('доход в день:', budgetDay, 'отстаток:', money % 30);
+
