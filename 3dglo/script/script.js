e77fab41b138b36cc34733a3a195cc614c3ce3d1
Christina Osipova <chosipova@gmail.com>
2019-07-25 22:58:40 +0300

    lesson 15

--- 3dglo/script/script.js
+++ 3dglo/script/script.js
@@ -274,4 +274,38 @@ window.addEventListener('DOMContentLoaded', function () {
   };
 
   slider();
+
+  const dataImgChange = () => {
+    const command = document.querySelector('#command');
+
+    let img;
+
+    command.addEventListener('mouseover', (event) => {
+      if (event.target.matches('.command__photo')) {
+        img = event.target.src;
+        event.target.src = event.target.dataset.img;
+      }
+    });
+
+    command.addEventListener('mouseout', (event) => {
+      if (event.target.matches('.command__photo')) {
+        event.target.dataset.img = event.target.src;
+        event.target.src = img;
+      }
+    });
+  };
+
+  dataImgChange();
+
+  const calcValue = () => {
+    const calcBlock = document.querySelector('.calc-block');
+
+    calcBlock.addEventListener('input', (event) => {
+      if (event.target.matches('.calc-item')) {
+        event.target.value = event.target.value.replace(/\D/, '');
+      }
+    });
+  };
+  calcValue();
+
 })
